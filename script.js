5c0e721df2ef664dd3406ab266add498b003552a
연호진 <72447026+Yeonlisa@users.noreply.github.com>
2020-11-12 22:17:32 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,84 +1,103 @@
-var firstNum, operator, previousKey, previousNum;
-// 위의 전역변수를 잘 활용하여, 계산기를 구현합니다.
+// [엘리먼트 변수]
+// 여기에 querySelector를 이용해 상호작용을 해야 하는 모든 엘리먼트를 전역변수로 지정하세요
+const btnSignup = document.querySelector('#btn-signup');
+const ErrorMessage = document.querySelector('#invalid-message');
 
-const calculator = document.querySelector('.calculator'); // calculator 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
-const buttons = calculator.querySelector('.calculator__buttons'); // calculator__keys 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
-const display = document.querySelector('.calculator__display'); // calculator__display 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
-// ! 위 코드(Line 1 - 6)는 수정하지 마세요.
+const userId = document.querySelectorAll('.input-form')[0];
+const phoneNumber = document.querySelectorAll('.input-form')[1];
+const confirmPhoneNumber = document.querySelectorAll('.input-form')[2];
 
-function calculate(n1, operator, n2) {
-  let result = '';
-    if(operator === '+'){
-      result = Number(n1)+Number(n2)
-    }
-    else if(operator === '-'){
-      result = Number(n1)-Number(n2)
-    }
-    else if(operator === '*'){
-      result = Number(n1)*Number(n2)
-    }
-    else if(operator === '/'){
-      result = Number(n1)/Number(n2)
-    }
-  // TODO : n1과 n2를 operator에 따라 계산하는 함수를 만드세요.
-  // ex) 입력값이 n1 : '1', operator : '+', n2 : '2' 인 경우, 3이 리턴됩니다.
-  return result;
+// [이벤트 핸들러]
+function handleBtnSignupClick() {
+  const inputs = [...document.querySelectorAll('.input-form')];
+  const areInputsValid = inputs.filter(function (input) {
+    return input.classList.contains('invalid');
+  });
+  if (areInputsValid.length !== 0) {
+    displayErrorMessage('회원가입 조건에 맞추어 작성해주세요.');
+  } else {
+    displayErrorMessage('회원가입이 완료되었습니다.');
+  }
+} 
+
+function handleUserIdInput(event) {
+  const target = event.target;
+  const parent = target.parentElement;
+  const value = target.value;
+  if (
+    !isEmpty(value) &&
+    onlyNumberAndEnglish(value) &&
+    moreThanLength(value, 5)
+  ) {
+    parent.classList.add('valid');
+    parent.querySelector('.message').textContent = '✔';
+  } else if (isEmpty(value)) {
+    parent.classList.add('invalid');
+    parent.querySelector('.message').textContent = '아이디를 5글자 이상 적어주세요.';
+  } else {
+    parent.classList.remove('valid');
+    parent.classList.remove('invalid');
+  }
 }
 
-buttons.addEventListener('click', function (event) {
-  // 버튼을 눌렀을 때 작동하는 함수입니다.
-
-  const target = event.target; // 클릭된 HTML 엘리먼트의 정보가 저장되어 있습니다.
-  const action = target.classList[0]; // 클릭된 HTML 엘리먼트에 클레스 정보를 가져옵니다.
-  const buttonContent = target.textContent; // 클릭된 HTML 엘리먼트의 텍스트 정보를 가져옵니다.
-  // ! 위 코드(Line 19 - 21)는 수정하지 마세요.
-
-  if (target.matches('button')) {
-    // TODO : 계산기가 작동할 수 있도록 아래 코드를 수정하세요. 작성되어 있는 조건문과 console.log를 활용하시면 쉽게 문제를 풀 수 있습니다.
-    // 클릭된 HTML 엘리먼트가 button이면
-    if (action === 'number') {
-      if (display.textContent === '0' || previousKey === 'operator'){
-      // 그리고 버튼의 클레스가 number이면
-      // 아래 코드가 작동됩니다.
-      display.textContent = '0';
-      console.log('숫자 ' + buttonContent + ' 버튼');
-      previousKey = 'number';
-      display.textContent = buttonContent;
-    } else if (previousKey === 'operator'){
-      firstNum = previousNum;
-    }
-      else if(display.textContent !== '0'){
-      display.textContent = display.textContent + buttonContent;
-    }
+function handlePhoneNumberInput(event) {
+  const target = event.target;
+  const parent = target.parentElement;
+  const value = target.value;
+  if (isPhoneNumber(value)) {
+    parent.classList.add('valid');
+    parent.classList.remove('invalid');
+    parent.querySelector('.message').textContent = '✔';
+  } else {
+    parent.classList.remove('valid');
+    parent.classList.add('invalid');
+    parent.querySelector('.message').textContent =
+      '핸드폰 번호는 000-0000-0000과 형식이 같아야 합니다.';
   }
+}
 
-    if (action === 'operator') {
-      console.log('연산자 ' + buttonContent + ' 버튼');
-      operator = buttonContent;
-      previousKey = 'operator';
-      firstNum=display.textContent;
-    }
-  
-
-    if (action === 'decimal') {
-      console.log('소수점 버튼');
-      previousKey = 'decimal';
-      display.textContent = display.textContent + '.'
-    }
-
-    if (action === 'clear') {
-      console.log('초기화 버튼');
-      previousKey = 'clear';
-      firstNum= undefined;
-      operator= undefined;
-      previousNum= undefined;
-      display.textContent = '0';
-    }
-
-    if (action === 'calculate') {
-      console.log('계산 버튼');
-      previousKey = 'calculate';
-      display.textContent = calculate(firstNum, operator, display.textContent);
-    }
+function handleConfirmPhoneNumberInput(event) {
+  const target = event.target;
+  const parent = target.parentElement;
+  const value = target.value;
+  const PhoneNumberValue = phoneNumber.querySelector('input').value;
+  if (PhoneNumberValue === value) {
+    parent.classList.add('valid');
+    parent.classList.remove('invalid');
+    parent.querySelector('.message').textContent = '✔';
+  } else {
+    parent.classList.remove('valid');
+    parent.classList.add('invalid');
+    parent.querySelector('.message').textContent =
+      '핸드폰 번호가 일치하지 않습니다.';
   }
-});
+}
+
+// [유효성 검증 함수]
+// 필요에 따라서 여러분들이 사용할 유효성 검증 함수를 추가하세요.
+function moreThanLength(str, n) {
+  return str.length >= n; // 항상 true 또는 false로 리턴하게 만드는 게 좋습니다.
+}
+
+function isHandlePhoneNumber(num){
+  return /^\d{3}-\d{3,4}-\d{4}$/.test(num);
+}
+
+function isEmpty(value) {
+  return value.length === 0;
+}
+
+function onlyNumberAndEnglish(str) {
+  return /^[A-Za-z][A-Za-z0-9]*$/.test(str);
+}
+// [시각적 피드백]
+function displayErrorMessage(message) {
+  ErrorMessage.classList.add('show');
+  ErrorMessage.textContent = message;
+}
+
+// [엘리먼트와 이벤트 핸들러의 연결]
+btnSignup.onclick = handleBtnSignupClick;
+userId.querySelector('input').onkeyup = handleUserIdInput;
+phoneNumber.querySelector('input').onkeyup = handlePhoneNumberInput;
+confirmPhoneNumber.querySelector('input').onkeyup = handleConfirmPhoneNumberInput;
